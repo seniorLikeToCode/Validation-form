7370b40bb021ebd39163deee9b395fa26c62cab7
Senior <abhishek234543@gmail.com>
2022-08-11 02:24:41 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 // import { BrowserRouter as Routes, Route, Link, Switch } from "react-router-dom";
 import Loan from './Components/LoansDetails/Loan';
